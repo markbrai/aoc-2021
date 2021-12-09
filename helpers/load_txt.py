08e6bec758cb43e7298462222589cd89d65a2e6d
Mark Braithwaite <mark.braith@gmail.com>
2021-12-09 20:36:31 +0000

    Day 4 started

--- helpers/load_txt.py
+++ helpers/load_txt.py
@@ -1,21 +1,46 @@
 from pathlib import Path
 
-def load_txt_to_list(day: int, line_type: str):
+def load_txt_to_list(day: int,
+                     line_type: str, 
+                     start: int = None, 
+                     end: int = None,
+                    name:str = "input") -> list:
     
     day_str = 'day' + str(day)
     
-    path_parts = [f"aoc-2021-{day_str}-input.txt"]
+    path_parts = [f"aoc-2021-{day_str}-{name}.txt"]
     file_path = Path.cwd().joinpath(*path_parts)
     
     with open(file_path, 'r') as input_file:
         
-        lines = input_file.readlines()
+        lines = input_file.readlines()[start:end]
         
         if line_type == "int":
             input_list = [int(i) for i in lines]
         elif line_type == "str":
             input_list = lines
+        elif line_type == "matrix":
+            input_list = get_matrix_from_txt(lines)
 
             
     return input_list
     
+def get_matrix_from_txt(lines: list) -> list:
+    
+    matrices = []
+    matrix = []
+    
+    lines = [line.strip().split(" ") for line in lines]
+    
+    for line in lines:
+        if line[0] != "":
+            row = [int(i) for i in line if i != ""]
+            matrix.append(row)
+            #print(row)
+
+        else:
+            #print("gap!")
+            matrices.append(matrix)
+            matrix = []
+            
+    return matrices
